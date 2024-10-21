ca6066c9326a300493a680e684447b16f9366b5d
WillDantasJPG <willgustavodantasadolpho@gmail.com>
2024-10-21 16:16:00 -0300

    feature:atualização do main

--- Infra/outputs.tf
+++ Infra/outputs.tf
@@ -1,11 +1,6 @@
-output "ecs_cluster_id" {
-  value = aws_ecs_cluster.ecs_cluster.id
-}
-
 output "ecs_service_name" {
   value = aws_ecs_service.analise_service.name
 }
-
 
 output "subnet_id" {
   description = "Subnet ID"
@@ -16,4 +11,3 @@ output "security_group_id" {
   description = "Security Group ID"
   value       = "sg-08a6c790338e94c72"
 }
-
