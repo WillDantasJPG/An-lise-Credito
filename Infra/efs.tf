bec2bb35c1bf13608728f7d3b98bccad4d1396ff
WillDantasJPG <willgustavodantasadolpho@gmail.com>
2024-10-21 12:32:58 -0300

    feature:atualização do main

--- Infra/efs.tf
+++ Infra/efs.tf
@@ -1,6 +1,5 @@
 resource "aws_efs_file_system" "my_efs" {
-  creation_token = "my-efs-file-system"  # Nome único para o sistema de arquivos
-  performance_mode = "generalPurpose"      # Modo de desempenho
+  creation_token = "my-efs-file-system-${timestamp()}"
 
   lifecycle {
     prevent_destroy = true
@@ -8,7 +7,7 @@ resource "aws_efs_file_system" "my_efs" {
 }
 
 resource "aws_efs_mount_target" "my_efs_mount" {
-  file_system_id = aws_efs_file_system.my_efs.id  # ID do sistema de arquivos EFS
-     subnet_id      = "subnet-0123456789abcdef0"  # Substitua pelo ID da sua sub-rede
-     security_groups = ["sg-0123456789abcdef0"]    # Substitua pelo ID do seu grupo de segurança
+  file_system_id  = aws_efs_file_system.my_efs.id
+  subnet_id       = "subnet-0123456789abcdef0"
+  security_groups  = ["sg-0123456789abcdef0"]
 }
--- Infra/outputs.tf
+++ Infra/outputs.tf
@@ -9,11 +9,11 @@ output "ecs_service_name" {
 
 output "subnet_id" {
   description = "Subnet ID"
-  value       = "subnet-09424067824895155"  # Você pode usar uma variável ou referência de recurso aqui
+  value       = "subnet-09424067824895155"
 }
 
 output "security_group_id" {
   description = "Security Group ID"
-  value       = "sg-08a6c790338e94c72"  # Você pode usar uma variável ou referência de recurso aqui
+  value       = "sg-08a6c790338e94c72"
 }
 
